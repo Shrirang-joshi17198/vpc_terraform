9a3980f1ee4b1615c98d5efece2d2022c278a862
Shrirang Joshi <98954152+Shrirang-joshi17198@users.noreply.github.com>
2025-02-06 10:32:53 +0530

    Delete aws_module directory

--- aws_module/ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the created EC2 instance"
-  value       = aws_instance.my_instance.id
-}
-
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.my_instance.public_ip
-}
